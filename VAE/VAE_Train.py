0b703d7f854401d6f7ab4db12a766f1785ddeb67
basock2 <basock2@gmail.com>
2025-01-15 22:16:54 +0900

    modified:   VAE/VAE_Train.py 	new file:   VAE/__pycache__/VAE_Model.cpython-312.pyc 	new file:   VAE_generated/generated_0_ex0.png 	new file:   VAE_generated/generated_0_ex1.png 	new file:   VAE_generated/generated_0_ex2.png 	new file:   VAE_generated/generated_1_ex0.png 	new file:   VAE_generated/generated_1_ex1.png 	new file:   VAE_generated/generated_1_ex2.png 	new file:   VAE_generated/generated_2_ex0.png 	new file:   VAE_generated/generated_2_ex1.png 	new file:   VAE_generated/generated_2_ex2.png 	new file:   VAE_generated/generated_3_ex0.png 	new file:   VAE_generated/generated_3_ex1.png 	new file:   VAE_generated/generated_3_ex2.png 	new file:   VAE_generated/generated_4_ex0.png 	new file:   VAE_generated/generated_4_ex1.png 	new file:   VAE_generated/generated_4_ex2.png 	new file:   VAE_generated/generated_5_ex0.png 	new file:   VAE_generated/generated_5_ex1.png 	new file:   VAE_generated/generated_5_ex2.png 	new file:   VAE_generated/generated_6_ex0.png 	new file:   VAE_generated/generated_6_ex1.png 	new file:   VAE_generated/generated_6_ex2.png 	new file:   VAE_generated/generated_7_ex0.png 	new file:   VAE_generated/generated_7_ex1.png 	new file:   VAE_generated/generated_7_ex2.png 	new file:   VAE_generated/generated_8_ex0.png 	new file:   VAE_generated/generated_8_ex1.png 	new file:   VAE_generated/generated_8_ex2.png 	new file:   VAE_generated/generated_9_ex0.png 	new file:   VAE_generated/generated_9_ex1.png 	new file:   VAE_generated/generated_9_ex2.png 	new file:   dataset/MNIST/raw/t10k-images-idx3-ubyte 	new file:   dataset/MNIST/raw/t10k-images-idx3-ubyte.gz 	new file:   dataset/MNIST/raw/t10k-labels-idx1-ubyte 	new file:   dataset/MNIST/raw/t10k-labels-idx1-ubyte.gz 	new file:   dataset/MNIST/raw/train-images-idx3-ubyte 	new file:   dataset/MNIST/raw/train-images-idx3-ubyte.gz 	new file:   dataset/MNIST/raw/train-labels-idx1-ubyte 	new file:   dataset/MNIST/raw/train-labels-idx1-ubyte.gz

--- VAE/VAE_Train.py
+++ VAE/VAE_Train.py
@@ -11,9 +11,9 @@ DEVICE = torch.device("cpu")
 INPUT_DIM = 784
 H_DIM = 200
 Z_DIM = 20
-NUM_EPOCHS = 10
+NUM_EPOCHS = 3
 BATCH_SIZE = 32
-LR_RATE = 4
+LR_RATE = 0.0005
 
 # Dataset Loading
 dataset = datasets.MNIST(root="dataset/", train=True, transform=transforms.ToTensor(), download=True)
@@ -21,6 +21,7 @@ train_loader = DataLoader(dataset=dataset, batch_size=BATCH_SIZE, shuffle=True)
 model = VariationalAutoEncoder(INPUT_DIM, H_DIM, Z_DIM).to(DEVICE)
 optimizer = optim.Adam(model.parameters(), lr=LR_RATE)
 loss_fn = nn.BCELoss(reduction="sum")
+
 
 # Training
 for epoch in range(NUM_EPOCHS):
@@ -41,3 +42,20 @@ for epoch in range(NUM_EPOCHS):
         loss.backward()
         optimizer.step()
         loop.set_postfix(loss=loss.item())
+
+model = model.to("cpu")
+def inference(digit, num_example=1):
+    # take one example image from the dataset
+    for x, y in dataset:
+        if y == digit:
+            mu, sigma = model.encode(x.view(1,784))
+            for example in range(num_example):
+                epsilon = torch.rand_like(sigma)
+                z = mu + sigma * epsilon
+                out = model.decode(z)
+                out = out.view(-1, 1, 28, 28)
+                save_image(out, f"VAE_generated/generated_{digit}_ex{example}.png")
+            break
+    
+for idx in range(10):
+    inference(idx, num_example=3)
